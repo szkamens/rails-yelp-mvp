524083c26f1e0a5cc01b454d7280ad7e98b57d38
Schneur Kamensky <szkamens@gmail.com>
2020-05-12 18:03:04 +0300

    added review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,9 +18,9 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
-  
+
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
